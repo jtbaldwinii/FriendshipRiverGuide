84761efbb6b0b56004fa0b8098bf7ed63ea8a365
jtbaldwinii <39061767+jtbaldwinii@users.noreply.github.com>
2026-08-12 15:12:55 -0500

    Add bridge geometry data for river map

---
+++ data/bridges.js
@@ -0,0 +1,32 @@
+// Bridge centers used to draw actual crossings over the river schematic.
+// `branch` controls when the bridge is shown. `labelSide` alternates labels to reduce crowding.
+export const BRIDGES = [
+  {name:'Columbus Dr',branch:'main',lat:41.88848,lng:-87.62055,labelSide:1},
+  {name:'Michigan Ave',branch:'main',lat:41.88868,lng:-87.62455,labelSide:-1},
+  {name:'Wabash Ave',branch:'main',lat:41.88854,lng:-87.62615,labelSide:1},
+  {name:'State St',branch:'main',lat:41.88835,lng:-87.62772,labelSide:-1},
+  {name:'Dearborn St',branch:'main',lat:41.88815,lng:-87.62945,labelSide:1},
+  {name:'Clark St',branch:'main',lat:41.88798,lng:-87.63092,labelSide:-1},
+  {name:'LaSalle St',branch:'main',lat:41.88780,lng:-87.63235,labelSide:1},
+  {name:'Wells St',branch:'main',lat:41.88763,lng:-87.63385,labelSide:-1},
+  {name:'Franklin St',branch:'main',lat:41.88752,lng:-87.63545,labelSide:1},
+
+  {name:'Lake St',branch:'south',lat:41.88575,lng:-87.63805,labelSide:1},
+  {name:'Randolph St',branch:'south',lat:41.88445,lng:-87.63813,labelSide:-1},
+  {name:'Washington St',branch:'south',lat:41.88305,lng:-87.63817,labelSide:1},
+  {name:'Madison St',branch:'south',lat:41.88180,lng:-87.63810,labelSide:-1},
+  {name:'Monroe St',branch:'south',lat:41.88045,lng:-87.63796,labelSide:1},
+  {name:'Adams St',branch:'south',lat:41.87925,lng:-87.63778,labelSide:-1},
+  {name:'Jackson Blvd',branch:'south',lat:41.87815,lng:-87.63765,labelSide:1},
+  {name:'Van Buren St',branch:'south',lat:41.87640,lng:-87.63745,labelSide:-1},
+  {name:'Harrison St',branch:'south',lat:41.87480,lng:-87.63705,labelSide:1},
+  {name:'Roosevelt Rd',branch:'south',lat:41.86765,lng:-87.63525,labelSide:-1},
+  {name:'St. Charles Air Line',branch:'south',lat:41.86080,lng:-87.63440,labelSide:1},
+
+  {name:'Kinzie St RR',branch:'north',lat:41.88831,lng:-87.63901,labelSide:1},
+  {name:'Grand Ave',branch:'north',lat:41.89155,lng:-87.63925,labelSide:-1},
+  {name:'Chicago Ave',branch:'north',lat:41.89655,lng:-87.64100,labelSide:1},
+  {name:'Halsted St',branch:'north',lat:41.89955,lng:-87.64125,labelSide:-1},
+  {name:'Division St',branch:'north',lat:41.90305,lng:-87.64185,labelSide:1},
+  {name:'North Ave',branch:'north',lat:41.91046,lng:-87.65671,labelSide:-1}
+];
